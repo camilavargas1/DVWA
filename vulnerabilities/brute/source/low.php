48d992683526b60d12bcf31153dad3563e03ff4a
camilavargas1 <34197941+camilavargas1@users.noreply.github.com>
2024-11-26 14:08:36 -0500

    Update low.php

--- vulnerabilities/brute/source/low.php
+++ vulnerabilities/brute/source/low.php
@@ -9,7 +9,7 @@ if( isset( $_GET[ 'Login' ] ) ) {
 	$pass = md5( $pass );
 
 	// Check the database
-	$query  = "SELECT * FROM `users` WHERE user = '$user' AND password = '$pass';";
+	$query = "SELECT * FROM groups WHERE id=:group_id AND domain_id=:domain_id";
 	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
 
 	if( $result && mysqli_num_rows( $result ) == 1 ) {
